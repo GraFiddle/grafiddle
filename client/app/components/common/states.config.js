d0337b28ddafc88b2514fd229a218e4635e343e3
Manuel Grabowski <git@manuelgrabowski.de>
2015-04-30 23:31:23 +0200

    Embed view, modified states.config for new views

--- client/app/components/common/states.config.js
+++ client/app/components/common/states.config.js
@@ -13,11 +13,29 @@
                 }
             })
             .state('editor', {
+                url: '/edit',
+                views: {
+                    content: {
+                        templateUrl: 'components/editor/editor.html',
+                        controller: 'EditorController'
+                    }
+                }
+            })
+            .state('new', {
                 url: '/new',
                 views: {
                     content: {
                         templateUrl: 'components/editor/editor.html',
                         controller: 'EditorController'
+                    }
+                }
+            })
+            .state('embed', {
+                url: '/embed',
+                views: {
+                    content: {
+                        templateUrl: 'components/embed/embed.html',
+                        controller: 'EmbedController'
                     }
                 }
             });
---
+++ client/app/components/embed/embed-controller.js
@@ -0,0 +1,110 @@
+(function () {
+
+    function EmbedController($scope, $filter) {
+
+        $scope.embedView = 'graph';
+        
+        $scope.toggleInputEditor = function() {
+            $scope.dataEditorHidden = !$scope.dataEditorHidden;
+            $scope.dataInputHidden = !$scope.dataInputHidden;
+            if ($scope.SwitchButtonTitle == 'Manual data entry') {
+                $scope.SwitchButtonTitle = 'back to input dialog';
+            }
+            else {
+                $scope.SwitchButtonTitle = 'Manual data entry';
+            }
+        };
+
+
+        $scope.datasetString = '';
+        $scope.dataset = [
+            {
+                'day': '2013-01-02_00:00:00',
+                'sales': 3461.295202,
+                'income': 12365.053
+            },
+            {
+                'day': '2013-01-03_00:00:00',
+                'sales': 4461.295202,
+                'income': 13365.053
+            },
+            {
+                'day': '2013-01-04_00:00:00',
+                'sales': 4561.295202,
+                'income': 14365.053
+            }
+        ];
+
+        $scope.schema = {
+            day: {
+                type: 'datetime',
+                format: '%Y-%m-%d_%H:%M:%S',
+                name: 'Date'
+            }
+        };
+
+        $scope.optionsString = '';
+        $scope.options = {
+            rows: [{
+                key: 'income',
+                type: 'bar'
+            }, {
+                key: 'sales'
+            }],
+            xAxis: {
+                key: 'day',
+                displayFormat: '%Y-%m-%d %H:%M:%S'
+            }
+        };
+
+        // define code highlighting
+        $scope.optionsAceConfig = {
+            mode: 'json',
+            useWrapMode: false,
+            onLoad: function(_editor) {
+                _editor.setShowPrintMargin(false);
+            }
+        };
+
+        $scope.datasetAceConfig = {
+            mode: 'json',
+            useWrapMode: false,
+            onLoad: function(_editor) {
+                _editor.setShowPrintMargin(false);
+            }
+        };
+
+
+        $scope.$watch('options', function(json) {
+            $scope.optionsString = $filter('json')(json);
+        }, true);
+
+        $scope.$watch('optionsString', function(json) {
+            try {
+                $scope.options = JSON.parse(json);
+                $scope.wellFormedOptions = true;
+            } catch (e) {
+                $scope.wellFormedOptions = false;
+            }
+        }, true);
+
+        $scope.$watch('dataset', function(json) {
+            $scope.datasetString = $filter('json')(json);
+        }, true);
+
+        $scope.$watch('datasetString', function(json) {
+            try {
+                $scope.dataset = JSON.parse(json);
+                $scope.wellFormedDataset = true;
+            } catch (e) {
+                $scope.wellFormedDataset = false;
+            }
+        }, true);
+
+    }
+
+    angular
+        .module('grafiddle')
+        .controller('EmbedController', EmbedController);
+
+})();
